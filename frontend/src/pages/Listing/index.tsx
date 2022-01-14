6692df01737b60227c3074a558cb959638de5366
tadeu-sales <tadeu.sales.cursos@gmail.com>
2022-01-14 19:34:15 -0300

    sem movie

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -30,13 +30,7 @@ function Listing() {
             });
     }, [pageNumber]);
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+    
 
     const handlePageChange =(newPageNumber : number) => {
         setPagesNumber(newPageNumber);
